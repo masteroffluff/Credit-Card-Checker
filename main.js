c4152ade1f18308ebfdefbd145fe4b5f081f90d9
masteroffluff <131491594+masteroffluff@users.noreply.github.com>
2023-06-26 22:04:17 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,91 @@
+// All valid credit card numbers
+const valid1 = [4, 5, 3, 9, 6, 7, 7, 9, 0, 8, 0, 1, 6, 8, 0, 8]
+const valid2 = [5, 5, 3, 5, 7, 6, 6, 7, 6, 8, 7, 5, 1, 4, 3, 9]
+const valid3 = [3, 7, 1, 6, 1, 2, 0, 1, 9, 9, 8, 5, 2, 3, 6]
+const valid4 = [6, 0, 1, 1, 1, 4, 4, 3, 4, 0, 6, 8, 2, 9, 0, 5]
+const valid5 = [4, 5, 3, 9, 4, 0, 4, 9, 6, 7, 8, 6, 9, 6, 6, 6]
+
+// All invalid credit card numbers
+const invalid1 = [4, 5, 3, 2, 7, 7, 8, 7, 7, 1, 0, 9, 1, 7, 9, 5]
+const invalid2 = [5, 7, 9, 5, 5, 9, 3, 3, 9, 2, 1, 3, 4, 6, 4, 3]
+const invalid3 = [3, 7, 5, 7, 9, 6, 0, 8, 4, 4, 5, 9, 9, 1, 4]
+const invalid4 = [6, 0, 1, 1, 1, 2, 7, 9, 6, 1, 7, 7, 7, 9, 3, 5]
+const invalid5 = [5, 3, 8, 2, 0, 1, 9, 7, 7, 2, 8, 8, 3, 8, 5, 4]
+
+// Can be either valid or invalid
+const mystery1 = [3, 4, 4, 8, 0, 1, 9, 6, 8, 3, 0, 5, 4, 1, 4]
+const mystery2 = [5, 4, 6, 6, 1, 0, 0, 8, 6, 1, 6, 2, 0, 2, 3, 9]
+const mystery3 = [6, 0, 1, 1, 3, 7, 7, 0, 2, 0, 9, 6, 2, 6, 5, 6, 2, 0, 3]
+const mystery4 = [4, 9, 2, 9, 8, 7, 7, 1, 6, 9, 2, 1, 7, 0, 9, 3]
+const mystery5 = [4, 9, 1, 3, 5, 4, 0, 4, 6, 3, 0, 7, 2, 5, 2, 3]
+
+// An array of all the arrays above
+const batch = [valid1, valid2, valid3, valid4, valid5, invalid1, invalid2, invalid3, invalid4, invalid5, mystery1, mystery2, mystery3, mystery4, mystery5]
+
+
+// Add your functions below:
+
+// validate credit card number with Luhn algorithm
+function validateCred(arr){  
+    // set up a counter and placeholder for the checksum
+    let count = 0, checkSum = 0;
+    // loop through the array backwards
+    for(let i = arr.length -1;i>=0;i--){
+        // increment the counter
+        count++;
+        // if the counter is even multiply by 2 otherwise by 1 
+        let num = arr[i]*(2-(count%2));
+        // if 10 or over subtract 9
+        if (num>=10){num -=9};
+        // add the final number to the checksum
+        checkSum +=num;
+        
+
+    }
+    // true if the units digit of the checksum is 0 (checksum mod 10 is 0)
+    return checkSum%10===0;
+}
+
+function findInvalidCards(arr){
+    // filter to remove the valid cards
+    return arr.filter(arr1 =>!validateCred(arr1))
+}
+
+function getCompany(arr){
+    //simpekl switch statment to generate the company names
+    switch(arr[0]){
+        case 3 :
+            return 'Amex (American Express)';
+        case 4:
+            return 'Visa';
+        case 5:
+            return 'Mastercard'
+        case 6:  
+            return'Discover'
+        default:
+            return 'Company not found'
+
+    }
+}
+
+
+function idInvalidCardCompanies(arr){
+    // set up arry for output
+    let arrInvalComps = [];
+    // get the invalid cards
+    let arrInvalCards = findInvalidCards(arr);
+    // iterate through the invalid cards
+    arrInvalCards.forEach(card => {
+        // get the company
+        let cardComp = getCompany(card);
+        //check if the company is not in the oputput arry (and that the card comp isn't 'Company not found' as that would be silly.)
+        if(cardComp!='Company not found' && !arrInvalComps.some(comp=>comp===cardComp)){
+            // ifs push the comany name to the output array. 
+            arrInvalComps.push(cardComp);
+        }
+    });
+    // return our list of companies
+    return arrInvalComps;
+}
+
+console.log(idInvalidCardCompanies(batch));
